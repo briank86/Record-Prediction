c745d869e7e34c7392872a51de83e26590001d41
briank86 <143430894+briank86@users.noreply.github.com>
2023-09-06 15:47:57 -0400

    Add files via upload

---
+++ Predictions using Fangraphs.R
@@ -0,0 +1,74 @@
+library(ggplot2)
+library(dplyr)
+library(knitr)
+library(baseballr)
+
+# 
+# id <- playerid_lookup("Trout", "Mike")
+# fg_id <- id$fangraphs_id
+# fg_data_trout <- fg_batter_game_logs(playerid = fg_id, year = 2022)
+# 
+# 
+# fg_hleaders_data_2022 <- fg_batter_leaders(x = 2022, y = 2022)
+# 
+# id <- playerid_lookup("Eovaldi", "Nathan")
+# fg_id <- id$fangraphs_id
+# fg_data_eovaldi <- fg_pitcher_game_logs(playerid = fg_id, year = 2022)
+# 
+# fg_pleaders_data_2022 <- fg_pitcher_leaders(x = 2022, y = 2022)
+# 
+# fg_team_hit_2022 <- fg_team_batter(x = 2022, y = 2022)
+# 
+# fg_team_pitch_2022 <- fg_team_pitcher(x = 2022, y = 2022)
+# 
+# fg_milb_pitcher_game_logs()
+# 
+# fg_milb_batter_game_logs()
+# 
+# fg_park(2022)
+
+
+
+
+
+
+#2002 is the first year of all of the data being included
+
+#Fangraphs data I'm saving to do prediction work on advanced stats 
+batterTeamData <- fg_team_batter(x= 2002, y = 2019)
+batterTeamData <- rbind(batterTeamData, fg_team_batter(x=2021, y=2022))
+pitcherTeamData <- fg_team_pitcher(x= 2002, y = 2019)
+pitcherTeamData <- rbind(pitcherTeamData, fg_team_pitcher(x=2021, y=2022))
+
+hitting2023 <- fg_team_batter(x= 2023, y = 2023)
+pitching2023 <- fg_team_pitcher(x= 2023, y = 2023)
+
+write.csv(batterTeamData, file = 'C:\\Users\\brkea\\Desktop\\fg_team_hitting.csv')
+write.csv(pitcherTeamData, file = 'C:\\Users\\brkea\\Desktop\\fg_team_pitching.csv')
+
+write.csv(hitting2023, file = 'C:\\Users\\brkea\\Desktop\\fg_team_hitting_2023.csv')
+write.csv(pitching2023, file = 'C:\\Users\\brkea\\Desktop\\fg_team_pitching_2023.csv')
+
+
+
+
+
+#Making data set for the python file that was designed for espn data
+batterTeamData <- batterTeamData[,names(batterTeamData) %in% c("Team", "G", "AB", "R", "H", "2B", "3B", "HR", "RBI", "TB", "BB", "SO", "SB", "AVG", "OBP", "SLG", "OPS")]
+pitcherTeamData <- pitcherTeamData[,names(pitcherTeamData) %in% c("Team", "G", "W", "L", "ERA", "SV", "CG", "ShO", "IP", "H", "ER", "HR", "BB", "SO", "WHIP")]
+#doesn't have QS or OBA like espn
+
+hitting2023 <- hitting2023[,names(hitting2023) %in% c("Team", "G", "AB", "R", "H", "2B", "3B", "HR", "RBI", "TB", "BB", "SO", "SB", "AVG", "OBP", "SLG", "OPS")]
+pitching2023 <- pitching2023[,names(pitching2023) %in% c("Team","G", "W", "L", "ERA", "SV", "CG", "ShO", "IP", "H", "ER", "HR", "BB", "SO", "WHIP")]
+
+pitcherTeamData <- pitcherTeamData %>% rename("G" = "GP-pitch", "H"="H-pitch", "HR" = "HR-pitch", "BB"="BB-pitch", "SO"="SO-pitch")
+pitching2023 <- pitching2023 %>% rename("G" = "GP-pitch", "H"="H-pitch", "HR" = "HR-pitch", "BB"="BB-pitch", "SO"="SO-pitch")
+
+  
+write.csv(batterTeamData, file = 'C:\\Users\\brkea\\Desktop\\espn_team_hitting.csv')
+write.csv(pitcherTeamData, file = 'C:\\Users\\brkea\\Desktop\\espn_team_pitching.csv')
+
+write.csv(hitting2023, file = 'C:\\Users\\brkea\\Desktop\\espn_team_hitting_2023.csv')
+write.csv(pitching2023, file = 'C:\\Users\\brkea\\Desktop\\espn_team_pitching_2023.csv')
+
+
